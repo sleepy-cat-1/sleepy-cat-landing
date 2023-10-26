7a6c603912ffaea54147f34b6a43aa1bac538ac8
prescottc4 <106892591+prescottc4@users.noreply.github.com>
2023-10-26 02:12:29 +0000

    upadated

--- bansystemtest.js
+++ bansystemtest.js
@@ -1,4 +1,3 @@
-document.cookie = "banned=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
 if (document.cookie.split(';').some((item) => item.trim().startsWith('banned='))) {
-    // The user is banned, redirect them or show a message
+    window.location.href = "/banned.html";
 }
