e5a93a764d0f487af36d872e6ee7f76fe920651f
lunarr341 <144508249+lunarr341@users.noreply.github.com>
2025-05-28 18:05:06 +0000

    small patches. also oneko got a speed boost

--- styledata/oneko/oneko.js
+++ styledata/oneko/oneko.js
@@ -20,7 +20,7 @@
   let idleAnimation = null;
   let idleAnimationFrame = 0;
 
-  const nekoSpeed = 10;
+  const nekoSpeed = 40;
   const spriteSets = {
     idle: [[-3, -3]],
     alert: [[-7, -3]],
